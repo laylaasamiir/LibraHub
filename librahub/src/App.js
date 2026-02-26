0c6e24fc034fd8dd772edd1a8f088e5b407bba38
mennaabdeltawab07-dotcom <mennaabdeltawab07@gmail.com>
2026-02-26 23:42:27 +0200

    up

--- librahub/src/App.js
+++ librahub/src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { StudentProfile } from './Pages/StudentProfile';
+import Main from './Pages/Main';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<StudentProfile/> } />
+      <Route path='/Main' element={<Main/>}/>
       <Route path='*' element={<h1>404 Not Found</h1>} />
     </Routes>
   );
