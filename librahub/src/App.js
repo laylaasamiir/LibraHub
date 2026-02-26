d9a5e5b6d52222b2fbc7a3799a85595df52efddf
laylaasamiir <laaylaasamiir@gmail.com>
2026-02-26 23:04:08 +0200

    update

--- librahub/src/App.js
+++ librahub/src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { StudentProfile } from './Pages/StudentProfile';
 import Login from './Pages/Login';
-import StudentRegister from './Pages/StudentRegister';
+
+
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<StudentProfile/> } />
       <Route path='login' element={<Login/>}/>
-      <Route path='StudentRegister' element={<StudentRegister/>} />
+  
       <Route path='*' element={<h1>404 Not Found</h1>} />
       
     </Routes>
--- librahub/src/Pages/StudentRegister.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const StudentRegister = () => {
-  return (
-    <div>StudentRegister</div>
-  )
-}
