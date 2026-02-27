f3e4d367bd24aac0c14fff2ea75459e97c23668a
SalmaHossam23 <202327039@std.sci.cu.edu.eg>
2026-02-27 05:04:01 +0200

    Update Login.jsx

--- librahub/src/Pages/Login.jsx
+++ librahub/src/Pages/Login.jsx
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import './login.css';
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+const handleLogin = async (e) => {
+  e.preventDefault(); 
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    alert("Login successful ✅");
+    navigate("/StudentProfile"); 
+  } catch (error) {
+    alert(error.message);
+  }
+};
   return (
     <div className="login-page">
       <main className="login-main">
         <div className="login-container">
           <img src="/gradCap.jpg" alt="Graduation Cap" className="login-icon"/>
           <h1 className="login-title">Student Login</h1>
-          <form className="login-form" action="#" method="post">
+          <form className="login-form" onSubmit={handleLogin}>
             <label htmlFor="CollegeEmail" className="login-label">Email</label>
-            <input type="email" id="CollegeEmail" name="CollegeEmail" placeholder="Enter your email" required className="login-input"/>
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter your email" required className="login-input"/>
 
             <label htmlFor="password" className="login-label">Password</label>
-            <input type="password" id="password" name="password" placeholder="Enter your password" required className="login-input"/>
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter your password" required className="login-input"/>
 
             <button type="submit" className="login-button">Login</button>
           </form>
-          <p className="login-footer">Don't have an account? <a href="#" className="login-link">Register</a></p>
+          <p className="login-footer">Don't have an account? <Link to="/Register" className="login-link">Register</Link></p>
         </div>
       </main>
     </div>
