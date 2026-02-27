1e03681326c032e4072d6d8f174171fc8b662b9a
laylaasamiir <laaylaasamiir@gmail.com>
2026-02-27 20:23:51 +0200

    Update App.js

--- librahub/src/App.js
+++ librahub/src/App.js
@@ -5,6 +5,7 @@ import { StudentProfile } from './Pages/StudentProfile';
 import Borrow from './Pages/Borrow';
  
 import Login from './Pages/Login';
+import Register from './Pages/Register';
 
  
 
@@ -16,6 +17,7 @@ function App() {
       <Route path='/Borrow' element={<Borrow/>}/>
  
       <Route path='login' element={<Login/>}/>
+      <Route path='Register' element={<Register/>}/>
   
  
       <Route path='*' element={<h1>404 Not Found</h1>} />
