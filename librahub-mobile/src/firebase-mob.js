564c25661ba94e1d82e9242b1f54bcabedcd32f6
laylaasamiir <laaylaasamiir@gmail.com>
2026-02-27 16:40:54 +0200

    mob

---
+++ librahub-mobile/src/firebase-mob.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { getReactNativePersistence } from "firebase/auth/react-native";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAUQ1_Hv7rvLbD7jHKrA3a4DD_qe3ukGd4",
+    authDomain: "librahub-cd407.firebaseapp.com",
+    projectId: "librahub-cd407",
+    storageBucket: "librahub-cd407.firebasestorage.app",
+    messagingSenderId: "304810434717",
+    appId: "1:304810434717:web:942f26fdd3160440833474",
+    measurementId: "G-31WNMDN3HR"
+};
+
+// Initialize Firebase
+
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(AsyncStorage),
+});
+const db = getFirestore(app);
+export { app, auth, db };
