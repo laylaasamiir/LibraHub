54a34f0557d4a3fe7f1d356b3a2d4e790183ec51
laylaasamiir <laaylaasamiir@gmail.com>
2026-03-07 01:18:59 +0200

    m

--- librahub/src/Pages/Main.jsx
+++ librahub/src/Pages/Main.jsx
@@ -7,27 +7,20 @@ export const Main = () => {
    <>
    <div className="container">
         <h1>Welcome to LibraHub!</h1>
-        <h2>Are you?</h2>
+       
 
         <div className="boxs">
 
         
             <div className="light">
                 <img src='/Student.jpeg'  alt="Student"/>
-                <h3>Student</h3>
                 <Link to={"/login"}>
-                <button>Access Role</button>
+                <button>Get started</button>
                 </Link>
             </div>
 
     
-            <div className="dark">
-                <img src="Admin.jpeg" alt="Librarian"/>
-                <h3>Librarian / Admin</h3>
-                <Link to={"/login"}>
-                <button>Select Role</button>
-                </Link>
-            </div>
+       
 
         </div>
     </div>
