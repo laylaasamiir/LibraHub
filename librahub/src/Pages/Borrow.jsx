444f63e2006d4581f6a23d5f9233992234526ea2
laylaasamiir <laaylaasamiir@gmail.com>
2026-04-16 21:09:53 +0200

    u

--- librahub/src/Pages/Borrow.jsx
+++ librahub/src/Pages/Borrow.jsx
@@ -1,92 +1,96 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 import "./Borrow.css";
 import {
-  collection,
-  addDoc,
-  query,
-  where,
-  getDocs,
-  updateDoc,
-  doc,
-  serverTimestamp,
+    collection,
+    addDoc,
+    query,
+    where,
+    getDocs,
+    updateDoc,
+    doc,
+    serverTimestamp
 } from "firebase/firestore";
 import { FaUser, FaLock } from "react-icons/fa";
 import { auth, db } from "../firebase";
-import AdminRequests from "../components/AdminRequest";
+import AdminRequests from '../components/AdminRequest';
+ 
+
+ 
 
 const Borrow = () => {
-  const [borrowData, setBorrowData] = useState({
-    studentName: "",
-    studentCode: "",
-    bookCode: "",
-  });
-
-  const [messages, setMessage] = useState("");
-  const [color, setColor] = useState("");
-
-  const handleChange = (e) => {
-    setBorrowData({ ...borrowData, [e.target.name]: e.target.value });
-    setMessage("");
-    setColor("");
-  };
-
-  const handleBorrow = async (e) => {
-    e.preventDefault();
-
-    try {
-      const booksRef = collection(db, "books");
-      const q = query(
-        booksRef,
-        where("bookId", "==", Number(borrowData.bookCode))
-      );
-
-      const querySnapshot = await getDocs(q);
-
-      if (querySnapshot.empty) {
-        setMessage("No book found with the provided code.");
-        setColor("red");
-        return;
-      }
-
-      const bookDoc = querySnapshot.docs[0];
-      const bookData = bookDoc.data();
-
-      if (bookData.isBorrowed === true) {
-        setMessage("This book is already borrowed.");
-        setColor("#ffc107");
-        return;
-      }
-
-      await addDoc(collection(db, "borrowedBooks"), {
-        studentName: borrowData.studentName,
-        studentCode: borrowData.studentCode,
-        userId: auth.currentUser.uid,
-        bookCode: Number(borrowData.bookCode),
-        bookDocId: bookDoc.id,
-        bookTitle: bookData.title,
-        borrowedAt: serverTimestamp(),
-        status: "borrowed",
-      });
-
-      await updateDoc(doc(db, "books", bookDoc.id), {
-        isBorrowed: true,
-        borrowedByCode: borrowData.studentCode,
-      });
-
-      setMessage("Book borrowed successfully!");
-      setColor("green");
-
-      setBorrowData({
+    
+ 
+    
+    const [borrowData, setBorrowData] = useState({
         studentName: "",
         studentCode: "",
         bookCode: "",
-      });
-    } catch (error) {
-      console.error("Error borrowing book:", error);
-      alert("Failed to borrow book.");
-    }
-  };
+    });
+    const [messages, setMessage] = useState("");
+    const [color, setColor] = useState("");
 
+    const handleChange = (e) => {
+        setBorrowData({ ...borrowData, [e.target.name]: e.target.value });
+        setMessage("");
+        setColor("");
+    };
+
+    const handleBorrow = async (e) => {
+        e.preventDefault();
+
+        try {
+            const booksRef = collection(db, "books");
+            const q = query(
+                booksRef,
+                where("bookId", "==", Number(borrowData.bookCode))
+            );
+            const querySnapshot = await getDocs(q);
+            if (querySnapshot.empty) {
+                setMessage("No book found with the provided code.");
+                setColor("red");
+                return;
+            }
+
+
+            const bookDoc = querySnapshot.docs[0];
+            const bookData = bookDoc.data();
+            if (bookData.isBorrowed === true) {
+                setMessage("This book is already borrowed.");
+                setColor("#ffc107");
+                return;
+            }
+
+
+            await addDoc(collection(db, "borrowedBooks"), {
+                studentName: borrowData.studentName,
+                studentCode: borrowData.studentCode,
+                userId: auth.currentUser.uid,
+                bookCode: Number(borrowData.bookCode),
+                bookDocId: bookDoc.id,
+                bookTitle: bookData.title,
+                borrowedAt: serverTimestamp(),
+                status: "borrowed",
+            });
+
+
+            await updateDoc(doc(db, "books", bookDoc.id), {
+                isBorrowed: true,
+                borrowedByCode: borrowData.studentCode,
+            });
+
+            setMessage("Book borrowed successfully!");
+            setColor("green");
+
+            setBorrowData({
+                studentName: "",
+                studentCode: "",
+                bookCode: "",
+            });
+        } catch (error) {
+            console.error("Error borrowing book:", error);
+            alert("Failed to borrow book.");
+        }
+    };
   const handleReturn = async () => {
     try {
       const booksRef = collection(db, "books");
@@ -149,9 +153,14 @@ const Borrow = () => {
     }
   };
 
-  return (
-    <>
-      <div className="borrow-page">
+
+
+    return (
+        <>
+ 
+
+
+          <div className="borrow-page">
         <div className="borrow-card">
           <h2>Borrow Book</h2>
 
@@ -172,49 +181,48 @@ const Borrow = () => {
                 name="studentName"
                 value={borrowData.studentName}
                 onChange={handleChange}
-              />
+                            />
+                        </div>
+                        <div className="input-box">
+                            <FaUser className="icon" />
+                            <input
+                                type="text"
+                                placeholder="Enter Student code..."
+                                name='studentCode'
+                                value={borrowData.studentCode}
+                                onChange={handleChange}
+                            />
+                        </div>
+
+                    </div>
+                    <label>Enter Book Copy ID:</label>
+                    <div className='input-box'>
+                        <FaLock className="icon" />
+
+                        <input
+                            type="text"
+                            placeholder="Enter Book Code..."
+                            name='bookCode'
+                            value={borrowData.bookCode}
+                            onChange={handleChange}
+                        />
+                    </div>
+
+
+                    <br />
+                    <div className="buttons">
+                        <button className="confirm" onClick={handleBorrow}>Confirm Borrow</button>
+                        <button className="return" onClick={handleReturn}>Return</button>
+                    </div>
+                </div>
             </div>
+            <AdminRequests/>
 
-            <div className="input-box">
-              <FaUser className="icon" />
-              <input
-                type="text"
-                placeholder="Enter Student code..."
-                name="studentCode"
-                value={borrowData.studentCode}
-                onChange={handleChange}
-              />
-            </div>
-          </div>
 
-          <label>Enter Book Copy ID:</label>
-          <div className="input-box">
-            <FaLock className="icon" />
-            <input
-              type="text"
-              placeholder="Enter Book Code..."
-              name="bookCode"
-              value={borrowData.bookCode}
-              onChange={handleChange}
-            />
-          </div>
 
-          <br />
 
-          <div className="buttons">
-            <button className="confirm" onClick={handleBorrow}>
-              Confirm Borrow
-            </button>
-            <button className="return" onClick={handleReturn}>
-              Return
-            </button>
-          </div>
-        </div>
-      </div>
+        </>
+    )
+}
 
-      <AdminRequests />
-    </>
-  );
-};
-
-export default Borrow;
+export default Borrow
