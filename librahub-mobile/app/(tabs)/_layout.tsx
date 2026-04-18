149422989a9d22c62c5216b60bcff4aa3feac2a1
MarwaRabea <marwarabiea180@gmail.com>
2026-04-18 12:14:38 +0200

    updates

--- librahub-mobile/app/(tabs)/_layout.tsx
+++ librahub-mobile/app/(tabs)/_layout.tsx
@@ -10,26 +10,42 @@ export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+  <Tabs
+    screenOptions={{
+      tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+      headerShown: false,
+      tabBarButton: HapticTab,
+    }}
+  >
+    <Tabs.Screen
+      name="index"
+      options={{
+        title: "Home",
+        tabBarIcon: ({ color }) => (
+          <IconSymbol size={28} name="house.fill" color={color} />
+        ),
+      }}
+    />
+
+    <Tabs.Screen
+      name="explore"
+      options={{
+        title: "Explore",
+        tabBarIcon: ({ color }) => (
+          <IconSymbol size={28} name="paperplane.fill" color={color} />
+        ),
+      }}
+    />
+
+    <Tabs.Screen
+      name="profile"
+      options={{
+        title: "Profile",
+        tabBarIcon: ({ color }) => (
+          <IconSymbol size={28} name="person.fill" color={color} />
+        ),
+      }}
+    />
+  </Tabs>
+);
 }
